9524c0971e24307d43cfb07b31acaf7f35f91320
Evan Farmer <lkisaid@Evans-MacBook-Pro.local>
2018-08-23 00:26:02 -0400

    Updated Projects page
    Changed internal links to correct security issues

--- template-parts/content-page-projects.php
+++ template-parts/content-page-projects.php
@@ -9,6 +9,134 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <header class="entry-header">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </header><!-- .entry-header -->
+  <div class="blue-projects row">
+    <img src="/wp-content/uploads/2016/11/blue-projects.jpg" alt="Mac Background and Coffee" width="1400" height="560" />
+  </div>
+  &nbsp;
+  <br>
+  <div class="projects entry-content">
+    <h1 class="projects-title">Web Projects</h1>
+    <hr>
+    <br>
+    <div class="row">
+      <div class="essa-arts small-12 large-4 columns">
+        &nbsp;
+        <a href="https://www.essarts.com"><h3>Essa Arts</h3></a>
+        <a href="https://www.essarts.com"><img class="essa-arts" src="/wp-content/uploads/2018/08/essa-arts-site.jpg" alt="Essa Arts Site" width="500" height="400"/></a>
+      </div>
+      <div class="essa-arts-info small-12 large-4 columns">
+        <h4>The Essa Arts website is a digital representation of the work and studio offerings of Sheryl Amburgey. Essa Arts has classes for children, art parties, community events, and features works by other local artists.</h4><br><hr><br><h4>Being somewhat of a "brochure" site, this web page is simple and informative, and features a Facebook event calendar!</h4><a href="https://www.essarts.com"><h4 class="check-it"><< Check it out! >></h4></a> 
+      </div>
+      <div class="essa-arts-events small-12 large-4 columns">
+        &nbsp;
+        <a href="https://www.essaarts.com"><img class="essa-arts" src="/wp-content/uploads/2018/08/essa-arts-blog.jpg" alt="Essa Arts Blog" width="500" height="400"/></a>
+      </div>
+    </div>
+    <div class="essa-arts-origin row">
+      <div class="large-3 columns"><h2></h2>
+      </div>
+      <div class="large-6 columns"><br>
+      <h4>The new and improved Essa Arts website was created with a child theme of <a href="https://wordpress.org/themes/lighthouse/"><strong>"Lighthouse"</strong></a> then we custom coded each page, and chose photos and colors that compliment both the Essa design and Sheryl's creative flair.</h4><br>
+      </div>
+     <br>
+     <hr>
+     <br>
+    <div class="row">
+      <div class="red-cedar small-12 large-4 columns">
+        &nbsp;
+        &nbsp;
+        <a href="http://www.redcedarbirth.com"><h3>Red Cedar Birth</h3></a>
+        <a href="http://www.redcedarbirth.com"><img class="red-cedar" src="/wp-content/uploads/2016/10/red-cedar-site.jpg" alt="Red Cedar Site" width="500" height="400"/></a>
+      </div>
+      <div class="red-cedar-info small-12 large-4 columns">
+        <h4>The Red Cedar Birth site was developed to serve as a resource for expectant mothers. RCB offers midwifery and doula services, and they sell herbal personal care products.</h4><br><hr><br><h4> This site features an ecommerce web store, a community resource page, and a Google-integrated event calendar.</h4><a href="http://www.redcedarbirth.com"><h4 class="check-it"><< Check it out! >></h4></a> 
+      </div>
+      <div class="red-cedar-store small-12 large-4 columns">
+        &nbsp;
+        <a href="http://www.redcedarbirth.com"><img class="red-cedar" src="/wp-content/uploads/2016/10/red-cedar-store.jpg" alt="Red Cedar Store" width="500" height="400"/></a>
+      </div>
+    </div>
+    <div class="red-cedar-origin row">
+      <div class="large-3 columns"><h2></h2>
+      </div>
+      <div class="large-6 columns">
+      <h4>The original Red Cedar website was hosted on WordPress.com with a basic theme. We created a custom theme from scratch, added the WooCommerce plugin to power the webstore, and developed a design and color scheme with a great designer.</h4>
+      </div>
+      <div class="webstore-mobile large-3 columns"><h2></h2>
+      </div>
+    </div>
+    <div class="more-soon">
+      &nbsp;
+      <h3>More portfolio pieces and project features coming soon!</h3>
+    </div>
+    <br>
+    <hr>
+    <br>
+    <h1 class="projects-title">Other Endeavors</h1>
+    <br>
+    <div class="row">
+      <div class="small-12 large-4 columns">
+        &nbsp;
+        <a href="https://www.meetup.com/WordPress-Ann-Arbor/"><h3>A2 WP Meetup</h3></a>
+        <a href="https://www.meetup.com/WordPress-Ann-Arbor/"><img class="wp-meetup" src="/wp-content/uploads/2016/11/A2WP.jpg" alt="A2 WP Meetup" width="500" height="350"/></a>
+      </div>
+      <div class="small-12 large-4 columns">
+        <a href="https://www.meetup.com/topics/wordpress/"><img class="wp-meetup" src="/wp-content/uploads/2016/10/wordpress-4-240.png" alt="WP Meetup" width="240" height="240"/></a>
+        <br>
+        <hr>
+        <br>
+        <h4>I'm involved in both the Ann Arbor and Jackson Michigan WordPress Meetup groups and would love to connect with others seeking to learn and grow.</h4>
+      </div>
+      &nbsp;
+      <div class="small-12 large-4 columns">
+        <a href="https://www.meetup.com/JXNWP-meetup/"><h3>JXN WP Meetup</h3></a>
+        <a href="https://www.meetup.com/JXNWP-meetup/"><img class="wp-meetup" src="/wp-content/uploads/2016/11/JXNWP.png" alt="Jackson WP Meetup" width="500" height="350"/></a>
+      </div>
+    </div> 
+    &nbsp; 
+    <div class="row">
+      <div class="small-12 large-12 columns">
+        <a href="https://2015.annarbor.wordcamp.org/"><h3>WordCamp A2</h3></a>
+          <a href="https://2015.annarbor.wordcamp.org/"><img class="wc2015" src="/wp-content/uploads/2016/11/WC2015.jpg"></a>
+          &nbsp;
+          <h4>I've helped organize, promote, and put together WordCamp Ann Arbor for the last couple of years...</h4><br>
+          <a href="https://2016.annarbor.wordcamp.org/"><img class="wc2016" src="/wp-content/uploads/2016/11/WC2016.jpg"></a><br>
+          <br><hr><br>
+          <h4>...and I was Sponsorship Wrangler for the first ever <a href="https://2017.jackson.wordcamp.org/"><strong>WordCamp JXN</strong></a> in 2017 and 2018!</h4><br>
+          <a href="https://2017.jackson.wordcamp.org/"><img class="wc2016" src="/wp-content/uploads/2017/05/wcjxn.png"></a>
+          &nbsp;
+      </div>
+    </div>
+    <hr>
+  </div><!-- .entry-content -->
+
+  <footer class="entry-footer">
+    <?php
+      edit_post_link(
+        sprintf(
+          /* translators: %s: Name of current post */
+          esc_html__( 'Edit %s', 'evanjfarmer-theme' ),
+          the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        ),
+        '<span class="edit-link">',
+        '</span>'
+      );
+    ?>
+  </footer><!-- .entry-footer -->
+</article><!-- #post-## --><?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Evanjfarmer_Theme
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
